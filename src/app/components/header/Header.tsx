ee64c2cfc2e36b87c2cff5a468d4ca546f7e9307
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-07-18 21:55:33 +0100

    added img home page

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./header.module.scss";
+import Link from "next/link";
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>Milchig</h1>
+            <Link href="/">MILCHIG</Link>
             <h3>PARAGON ARCADE</h3>
         </header>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import styles from "./page.module.scss";
-
+import Image from "next/image";
+import insideShop from "../../public/insideShop.webp";
 export default function Home() {
-    return <main className={styles.main}>Hello Milchig</main>;
+    return (
+        <main className={styles.main}>
+            <div className={styles.imgWrapper}>
+                <Image
+                    className={styles.insideShopImg}
+                    src={insideShop}
+                    alt="Inside Milchig"
+                    priority
+                />
+            </div>
+            <div className={styles.info}>
+                <p>BAKERY & COFFEE SHOP</p>
+                <p>Paragon Arcade</p>
+                <p>Unit 12</p>
+                <p>Hull HU1 3PQ</p>
+            </div>
+        </main>
+    );
 }
