0a1d331c21f2aa1578a6ee049d6886d2bd74644c
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-07-20 10:26:23 +0100

    insta reel

---
+++ src/app/components/instagram-embed/instagramEmbed.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+
+const InstagramEmbed = ({ url }) => {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "//www.instagram.com/embed.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <blockquote
+            className="instagram-media"
+            data-instgrm-captioned
+            data-instgrm-permalink={url}
+            data-instgrm-version="14"
+            style={{
+                background: "#FFF",
+                border: "0",
+                borderRadius: "3px",
+                boxShadow:
+                    "0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)",
+                margin: "1px",
+                maxWidth: "540px",
+                minWidth: "326px",
+                padding: "0",
+                width: "99.375%",
+            }}
+        >
+            <div style={{ padding: "16px" }}>
+                <a
+                    href={url}
+                    style={{
+                        background: "#FFFFFF",
+                        lineHeight: "0",
+                        padding: "0 0",
+                        textAlign: "center",
+                        textDecoration: "none",
+                        width: "100%",
+                    }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {/* Add any additional styles or elements as needed */}
+                </a>
+            </div>
+        </blockquote>
+    );
+};
+
+export default InstagramEmbed;
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,40 +1,18 @@
+"use client";
+import Link from "next/link";
+import InstagramEmbed from "../components/instagram-embed/instagramEmbed";
 import styles from "./page.module.scss";
 
 export default function Menu() {
     return (
         <main className={styles.main}>
-            <article className={styles.article}>
-                <section className={styles.openingTimes}>
-                    <p>COFFEE</p>
-                    <div className={styles.times}>
-                        <p className={styles.weekday}>MONDAY - FRIDAY</p>
-                        <p>09:00 - 15:00</p>
-                    </div>
-                    <div className={styles.times}>
-                        <p className={styles.sat}>SATURDAY</p>
-                        <p>09:00 - 15:00</p>
-                    </div>
-                    <div className={styles.times}>
-                        <p className={styles.sun}>SUNDAY</p>
-                        <p>CLOSED</p>
-                    </div>
-                </section>
-                <section className={styles.lunchTimes}>
-                    <p>LUNCHES & DINNERS</p>
-                    <div className={styles.times}>
-                        <p className={styles.weekday}>MONDAY - FRIDAY</p>
-                        <p>10:00 - 14:00</p>
-                    </div>
-                    <div className={styles.times}>
-                        <p className={styles.sat}>SATURDAY</p>
-                        <p>10:00 - 14:00</p>
-                    </div>
-                    <div className={styles.times}>
-                        <p className={styles.sun}>SUNDAY</p>
-                        <p>CLOSED</p>
-                    </div>
-                </section>
-            </article>
+            <Link href={"https://www.instagram.com/thisismilchig"}>
+                {" "}
+                CLICK HERE TO SEE OUR MOST RECENT MENU
+            </Link>
+            <div>
+                <InstagramEmbed url="https://www.instagram.com/reel/C4p1T7UNcwR/?utm_source=ig_embed&amp;utm_campaign=loading" />
+            </div>
         </main>
     );
 }
