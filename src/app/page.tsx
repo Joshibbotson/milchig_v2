70100b0933478ddded3a8c8b412fce27dc1ad6a0
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-07-28 19:04:51 +0100

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,13 @@ import insideShop from "../../public/insideShop.webp";
 export default function Home() {
     return (
         <main className={styles.main}>
-            {/* <div className={styles.imgWrapper}> */}
             <Image
                 className={styles.insideShopImg}
                 src={insideShop}
                 alt="Inside Milchig"
                 priority
             />
-            {/* </div> */}
+
             <div className={styles.info}>
                 <p>Paragon Arcade</p>
                 <p>Unit 12</p>
